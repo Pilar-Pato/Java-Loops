4eb195affec7b37e132d372d916d4b960e49f21e
PilarPato <fusionilusion@gmail.com>
2024-10-20 21:50:46 +0200

    Feature: LOOPS

--- src/main/java/org/factoriaf5/javaloops/Loops.java
+++ src/main/java/org/factoriaf5/javaloops/Loops.java
@@ -1,5 +1,12 @@
 package org.factoriaf5.javaloops;
 
 public class Loops {
+    public static String[] Tabla(int numero) {
+        String[] tabla = new String[10];
+        for (int i = 1; i <= 10; i++) {
+            tabla[i - 1] = numero + " x " + i + " = " + (numero * i);
+        }
+        return tabla;
+    }
 
 }
--- src/test/java/org/factoriaf5/javaloops/Loopstest.java
+++ src/test/java/org/factoriaf5/javaloops/Loopstest.java
@@ -1,5 +1,14 @@
 package org.factoriaf5.javaloops;
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
 
 public class Loopstest {
 
-}
+    @Test
+    public void testTabla() {
+        String[] resultado = Loops.Tabla(3);
+        String[] esperado = {"3 x 1 = 3", "3 x 2 = 6", "3 x 3 = 9", "3 x 4 = 12", "3 x 5 = 15", 
+                             "3 x 6 = 18", "3 x 7 = 21", "3 x 8 = 24", "3 x 9 = 27", "3 x 10 = 30"};
+        assertArrayEquals(esperado, resultado);
+    }
+}   
