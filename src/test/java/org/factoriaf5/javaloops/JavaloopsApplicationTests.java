44b7fa56f5d8880d406281e26de6e5264c4101ac
PilarPato <fusionilusion@gmail.com>
2024-10-20 20:49:55 +0200

    First commit

---
+++ src/test/java/org/factoriaf5/javaloops/JavaloopsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.javaloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaloopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
